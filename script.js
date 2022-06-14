47d932af9f34c4478a113c35525bc6a6eea99640
Aranha <anab.aranha@gmail.com>
2022-06-14 02:26:36 -0300

    Adding null to javascript function for number of cards in game

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function defineNumberOfCards(){
     numberSelected = prompt("Com quantas cartas você quer jogar? Escolha número par entre 4 e 14")
-    while (numberSelected%2 != 0 || numberSelected>14){
+    while (numberSelected%2 != 0 || numberSelected>14 || numberSelected == null || numberSelected == ''){
         alert("Escolha número par e entre 4 e 14!")
         numberSelected = prompt("Com quantas cartas você quer jogar? Escolha número par entre 4 e 14")
     }
+    // cardsArray = document.querySelectorAll(".card-game")
+    // console.log({cardsArray})
+    // for(i=0;i<numberSelected;i++){
+    //     cardsArray[i].removeClass('hidden')
+    // }
 }
 
 defineNumberOfCards();
-cardsArray = document.querySelectorAll(".card-game")
-console.log({cardsArray})
-
 
 function turnCard(element){
     back = element.querySelector("div:nth-child(1)")
