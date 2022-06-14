e70d4717a3a8192ccfd5105a663692c201e03159
Aranha <anab.aranha@gmail.com>
2022-06-14 17:02:34 -0300

    Adding the correct number of requested cards

--- script.js
+++ script.js
@@ -4,20 +4,19 @@ function defineNumberOfCards(){
         alert("Escolha número par e entre 4 e 14!")
         numberSelected = prompt("Com quantas cartas você quer jogar? Escolha número par entre 4 e 14")
     }
-    // cardsArray = document.querySelectorAll(".card-game")
-    // console.log({cardsArray})
-    // for(i=0;i<numberSelected;i++){
-    //     cardsArray[i].removeClass('hidden')
-    // }
+    cardsGame = document.querySelectorAll(".card-game.hidden")
+    for(let i=0;i<numberSelected;i++){
+        cardsGame[i].classList.remove("hidden")
+    }
 }
 
-defineNumberOfCards();
+defineNumberOfCards()
+
 
 function turnCard(element){
     back = element.querySelector("div:nth-child(1)")
     front = element.querySelector("div:nth-child(2)")
     cardFigure = front.innerHTML;
-    console.log({cardFigure})
     back.classList.toggle("front-face")
     front.classList.toggle("back-face-turn")   
     front.classList.toggle("back-face")
